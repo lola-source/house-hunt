bf897cc3c7e38f2ae08e2aa71475b68879e90406
lola-source <adagaladolores@gmail.com>
2023-10-07 17:27:41 +0300

    testrun

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: 'househunt.firebaseapp.com',
+  projectId: 'househunt',
+  storageBucket: 'househuunt.appspot.com',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
