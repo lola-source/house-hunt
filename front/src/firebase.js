658b36deeeb95a22f5a26a6f8b190b877d19e503
lola-source <adagaladolores@gmail.com>
2023-10-08 15:07:43 +0300

    firebase

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'househunt.firebaseapp.com',
-  projectId: 'househunt',
-  storageBucket: 'househuunt.appspot.com',
-  messagingSenderId: '',
-  appId: '',
+  authDomain: "househunt-2f716.firebaseapp.com",
+  projectId: "househunt-2f716",
+  storageBucket: "househunt-2f716.appspot.com",
+  messagingSenderId: "913954030258",
+  appId: "1:913954030258:web:50e38771aeb0dd088effab",
+  measurementId: "G-X5XGSBH8D6"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
